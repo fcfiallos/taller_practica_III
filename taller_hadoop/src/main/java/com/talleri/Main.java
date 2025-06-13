aa972b99b0c2c5f2fe33af8ac630cd756572100c
fcfiallos <fcfiallos@uce.edu.ec>
2025-06-12 22:12:18 -0500

    codigo sin uso eliminacion

--- taller_hadoop/src/main/java/com/talleri/Main.java
+++
@@ -1,8 +0,0 @@
-package com.talleri;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Este es el lanzador de jobs MapReduce. Ejecuta los jobs directamente con: ");
-        System.out.println("hadoop jar target/taller_hadoop-1.0-SNAPSHOT.jar com.talleri.reddit.RedditJob /ruta/input /ruta/output");
-    }
-}
