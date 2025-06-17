64377938a4c221d8ab0f8b99aa8c63460813f114
fcfiallos <fcfiallos@uce.edu.ec>
2025-06-16 21:38:00 -0500

    hdfs cargar imagenes

--- api/app/main.py
+++ api/app/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, File, UploadFile
 from app.utils import preprocess_image
 from pydantic import BaseModel, Field
 from typing import Literal 
-
+from hdfs import InsecureClient
+import datetime
 
 from app.model_loader import predict_emotion
 from app.text_model_handler import predict_depression
@@ -42,6 +43,16 @@ async def predict(imagen: UploadFile = File(...)):
     image_bytes = await imagen.read()
     image_array = preprocess_image(image_bytes)
     emotion = predict_emotion(image_array)
+
+    # Guardar imagen cruda en HDFS
+    hdfs_url = "http://namenode:9870"  # URL del namenode expuesto en docker-compose
+    client = InsecureClient(hdfs_url, user='root')
+    now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    hdfs_path = f"/nuevo/{emotion}/{now}_{imagen.filename}"
+    # Crea la carpeta si no existe y sube la imagen
+    with client.write(hdfs_path, overwrite=True) as writer:
+        writer.write(image_bytes)
+
     return {"emoción": emotion}
 
 # endopoitn para el modelo de texto
